595f24b32876688fef76e16bb7c3493b4af14e66
ericgao1997 <31947627+ericgao1997@users.noreply.github.com>
2020-12-10 13:39:46 -0800

    Adding team's work so far

---
+++ datacleaning.py
@@ -0,0 +1,34 @@
+import sys, string, re
+from pyspark.sql import SparkSession, functions, types
+from pyspark.sql.functions import *
+
+
+spark = SparkSession.builder.appName('datacleaning').getOrCreate()
+spark.sparkContext.setLogLevel('ERROR')
+# {OFF, FATAL, ERROR, WARN, INFO, DEBUG, TRACE, ALL}
+
+assert sys.version_info >= (3, 5)
+assert spark.version >= '2.3'
+
+OUTPUT_DIRECTORY = "out"
+
+# python3 datacleaning.py osm/amenities-vancouver.json.gz
+
+def main(input_file):
+  data = spark.read.json(input_file)
+  # json of form in SCHEMA.txt
+  # https://wiki.openstreetmap.org/wiki/Tags
+  # off the top of my head, I imagine tags we'd keep are something like:
+
+tagsample = data[s]
+  
+  data.printSchema()
+  # flat = 
+  # data.write.option("header", "true").mode("overwrite").csv(OUTPUT_DIRECTORY)
+
+  # TODO
+  
+  
+if __name__=='__main__':
+  input_file = sys.argv[1]
+  main(input_file)         
---
+++ readme.md
@@ -0,0 +1,18 @@
+#OSM, Photos, and Tours
+This is our final submission for CMPT, in which we [TBA]
+
+##Usage
+###Samples
+WIP
+
+###Full Demo
+To run the whole thing locally, start to finish, using the [`amenities-vancouver.json.gz`]:https://coursys.sfu.ca/2020fa-cmpt-353-d1/pages/ProjectTourData data sample from the assignment page: 
+```
+
+```
+
+##Requirements
+* Python 3.5+
+* Spark 2.3+
+
+WIP
