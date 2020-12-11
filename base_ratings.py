effcd1db52a60b498b2539f46a9aea562d7f8bb0
ericgao1997 <31947627+ericgao1997@users.noreply.github.com>
2020-12-11 00:58:13 -0800

    Heatmap dummy generation added

--- base_ratings.py
+++ base_ratings.py
@@ -4,8 +4,8 @@ import csv
 import matplotlib.pyplot as plt
 import sys
 
-def give_rating(amenity, ratings):
-    return ratings[amenity]
+def give_rating(row, ratings):
+    return ratings[row['amenity']]*len(row['tags'])
 
 def main(data_file, rating_file):
     data = pd.read_json(data_file, lines=True) 
@@ -13,9 +13,10 @@ def main(data_file, rating_file):
         reader = csv.reader(infile)
         ratings = {rows[0]:int(rows[1]) for rows in reader}
     print (ratings)
-    data['risk'] = data.apply(lambda row: give_rating(row['amenity'],ratings), axis=1)
+    data['risk'] = data.apply(lambda row: give_rating(row,ratings), axis=1)
     data = data[data['risk']>1]
     print(data['amenity'],data['risk'])
+    data = data.drop('tags', axis=1)
     data.to_csv("basic_risk.csv",index=False)
 
 if __name__=='__main__':
