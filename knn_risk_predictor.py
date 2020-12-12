55ffd6c93d78eb6704c049c58fcf6366ba2617bb
ericgao1997 <31947627+ericgao1997@users.noreply.github.com>
2020-12-11 20:43:28 -0800

    smart

--- knn_risk_predictor.py
+++ knn_risk_predictor.py
@@ -10,11 +10,14 @@ def risk_calc(row):
     base_risk = row['medical'] + row['food']*0.8 + row['gathering']*0.5 + row['transport']*0.5 + row['notable']*0.2
     if base_risk == 0:
         return 0
-    t_total = row['medical'] + row['food'] + row['gathering']+ row['transport'] + row['notable']
+    # t_total = row['medical'] + row['food'] + row['gathering']+ row['transport'] + row['notable']
+    t_total = 1
     return base_risk**t_total
 
 def irl_risk(model):
-    data = pd.read_csv("data/confirmed_cases.csv") 
+    irl_cases = pd.read_csv("data/confirmed_cases.csv") 
+    irl_cases['risk'] = model.predict(irl_cases)
+    irl_cases.to_csv("out/irl_risk.csv")
 
 def main(data_file):
     data = pd.read_csv(data_file) 
@@ -48,6 +51,8 @@ def main(data_file):
     overall_risks = pd.concat([known_2,new_targets])
 
     overall_risks.to_csv('out/smart_risks.csv',index = False)
+
+    irl_risk(wider_neigh)
     # print (overall_risks)
     # targets = data[ (data['food']==False) | (data['medical']==False) | (data['gathering']==False) | (data['transport']==False) | (data['notable']==False)  ]
     # targets = targets[targets['tag_count']>0]
